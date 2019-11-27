20508c67a91015f00f51597fb2cf0ef4087780ab
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-28 00:53:05 +0330

    Node class documented

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -1,18 +1,39 @@
 package Node;
 
+/**
+ * @author baha2r
+ * Date: 28/Nov/2019 10:38 PM
+ **/
+
 public class Node {
     public int x;
     public int y;
 
+    /**
+     * Constructor
+     *
+     * @param x X coordination of a Node
+     * @param y Y coordination of a Node
+     */
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns position of X
+     *
+     * @return Returns position of X
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Returns position of Y
+     *
+     * @return Returns position of Y
+     */
     public int getY() {
         return y;
     }
