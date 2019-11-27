bebbd6f3f32c2749f9ec94d02342be3453f2bb28
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-27 23:47:35 +0330

    Node class created and completed

---
+++ src/Node/Node.java
@@ -0,0 +1,19 @@
+package Node;
+
+public class Node {
+    public int x;
+    public int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
