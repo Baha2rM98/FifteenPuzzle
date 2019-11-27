8e0da65bc8909e54180189d46f5649fb60d742c2
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-27 23:41:58 +0330

    !

--- src/FifteenPuzzle.java
+++ src/FifteenPuzzle.java
@@ -1,10 +1,19 @@
-import java.util.*;
+package algostudy;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Illustrates how to solve the fifteen puzzle using Dijkstra's algorithm and A*.
  * Haven't really written any java in at least 5 years, so apologies for sloppiness.
  *
  * @author leodirac
+ *
  */
 public class FifteenPuzzle {
 
@@ -13,39 +22,39 @@ public class FifteenPuzzle {
         public int y;
 
         public TilePos(int x, int y) {
-            this.x = x;
-            this.y = y;
+            this.x=x;
+            this.y=y;
         }
 
     }
 
-    public final static int DIMS = 4;
+    public final static int DIMS=4;
     private int[][] tiles;
     private int display_width;
     private TilePos blank;
 
     public FifteenPuzzle() {
         tiles = new int[DIMS][DIMS];
-        int cnt = 1;
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                tiles[i][j] = cnt;
+        int cnt=1;
+        for(int i=0; i<DIMS; i++) {
+            for(int j=0; j<DIMS; j++) {
+                tiles[i][j]=cnt;
                 cnt++;
             }
         }
-        display_width = Integer.toString(cnt).length();
+        display_width=Integer.toString(cnt).length();
 
         // init blank
-        blank = new TilePos(DIMS - 1, DIMS - 1);
-        tiles[blank.x][blank.y] = 0;
+        blank = new TilePos(DIMS-1,DIMS-1);
+        tiles[blank.x][blank.y]=0;
     }
 
-    public final static FifteenPuzzle SOLVED = new FifteenPuzzle();
+    public final static FifteenPuzzle SOLVED=new FifteenPuzzle();
 
 
     public FifteenPuzzle(FifteenPuzzle toClone) {
         this();  // chain to basic init
-        for (TilePos p : allTilePos()) {
+        for(TilePos p: allTilePos()) {
             tiles[p.x][p.y] = toClone.tile(p);
         }
         blank = toClone.getBlank();
@@ -53,9 +62,9 @@ public class FifteenPuzzle {
 
     public List<TilePos> allTilePos() {
         ArrayList<TilePos> out = new ArrayList<TilePos>();
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                out.add(new TilePos(i, j));
+        for(int i=0; i<DIMS; i++) {
+            for(int j=0; j<DIMS; j++) {
+                out.add(new TilePos(i,j));
             }
         }
         return out;
@@ -73,8 +82,8 @@ public class FifteenPuzzle {
 
 
     public TilePos whereIs(int x) {
-        for (TilePos p : allTilePos()) {
-            if (tile(p) == x) {
+        for(TilePos p: allTilePos()) {
+            if( tile(p) == x ) {
                 return p;
             }
         }
@@ -84,9 +93,9 @@ public class FifteenPuzzle {
 
     @Override
     public boolean equals(Object o) {
-        if (o instanceof FifteenPuzzle) {
-            for (TilePos p : allTilePos()) {
-                if (this.tile(p) != ((FifteenPuzzle) o).tile(p)) {
+        if(o instanceof FifteenPuzzle) {
+            for(TilePos p: allTilePos()) {
+                if( this.tile(p) != ((FifteenPuzzle) o).tile(p)) {
                     return false;
                 }
             }
@@ -98,26 +107,27 @@ public class FifteenPuzzle {
 
     @Override
     public int hashCode() {
-        int out = 0;
-        for (TilePos p : allTilePos()) {
-            out = (out * DIMS * DIMS) + this.tile(p);
+        int out=0;
+        for(TilePos p: allTilePos()) {
+            out= (out*DIMS*DIMS) + this.tile(p);
         }
         return out;
     }
 
+
     public void show() {
         System.out.println("-----------------");
-        for (int i = 0; i < DIMS; i++) {
+        for(int i=0; i<DIMS; i++) {
             System.out.print("| ");
-            for (int j = 0; j < DIMS; j++) {
+            for(int j=0; j<DIMS; j++) {
                 int n = tiles[i][j];
                 String s;
-                if (n > 0) {
+                if( n>0) {
                     s = Integer.toString(n);
                 } else {
                     s = "";
                 }
-                while (s.length() < display_width) {
+                while( s.length() < display_width ) {
                     s += " ";
                 }
                 System.out.print(s + "| ");
@@ -130,10 +140,10 @@ public class FifteenPuzzle {
 
     public List<TilePos> allValidMoves() {
         ArrayList<TilePos> out = new ArrayList<TilePos>();
-        for (int dx = -1; dx < 2; dx++) {
-            for (int dy = -1; dy < 2; dy++) {
+        for(int dx=-1; dx<2; dx++) {
+            for(int dy=-1; dy<2; dy++) {
                 TilePos tp = new TilePos(blank.x + dx, blank.y + dy);
-                if (isValidMove(tp)) {
+                if( isValidMove(tp) ) {
                     out.add(tp);
                 }
             }
@@ -143,15 +153,15 @@ public class FifteenPuzzle {
 
 
     public boolean isValidMove(TilePos p) {
-        if ((p.x < 0) || (p.x >= DIMS)) {
+        if( ( p.x < 0) || (p.x >= DIMS) ) {
             return false;
         }
-        if ((p.y < 0) || (p.y >= DIMS)) {
+        if( ( p.y < 0) || (p.y >= DIMS) ) {
             return false;
         }
         int dx = blank.x - p.x;
         int dy = blank.y - p.y;
-        if ((Math.abs(dx) + Math.abs(dy) != 1) || (dx * dy != 0)) {
+        if( (Math.abs(dx) + Math.abs(dy) != 1 ) || (dx*dy != 0) ) {
             return false;
         }
         return true;
@@ -159,19 +169,18 @@ public class FifteenPuzzle {
 
 
     public void move(TilePos p) {
-        if (!isValidMove(p)) {
+        if( !isValidMove(p) ) {
             throw new RuntimeException("Invalid move");
         }
-        assert tiles[blank.x][blank.y] == 0;
+        assert tiles[blank.x][blank.y]==0;
         tiles[blank.x][blank.y] = tiles[p.x][p.y];
-        tiles[p.x][p.y] = 0;
+        tiles[p.x][p.y]=0;
         blank = p;
     }
 
 
     /**
      * returns a new puzzle with the move applied
-     *
      * @param p
      * @return
      */
@@ -183,9 +192,9 @@ public class FifteenPuzzle {
 
 
     public void shuffle(int howmany) {
-        for (int i = 0; i < howmany; i++) {
+        for(int i=0; i<howmany; i++) {
             List<TilePos> possible = allValidMoves();
-            int which = (int) (Math.random() * possible.size());
+            int which =  (int) (Math.random() * possible.size());
             TilePos move = possible.get(which);
             this.move(move);
         }
@@ -193,15 +202,15 @@ public class FifteenPuzzle {
 
 
     public void shuffle() {
-        shuffle(DIMS * DIMS * DIMS * DIMS * DIMS);
+        shuffle(DIMS*DIMS*DIMS*DIMS*DIMS);
     }
 
 
     public int numberMisplacedTiles() {
-        int wrong = 0;
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                if ((tiles[i][j] > 0) && (tiles[i][j] != SOLVED.tiles[i][j])) {
+        int wrong=0;
+        for(int i=0; i<DIMS; i++) {
+            for(int j=0; j<DIMS; j++) {
+                if( (tiles[i][j] >0) && ( tiles[i][j] != SOLVED.tiles[i][j] ) ){
                     wrong++;
                 }
             }
@@ -218,17 +227,16 @@ public class FifteenPuzzle {
     /**
      * another A* heuristic.
      * Total manhattan distance (L1 norm) from each non-blank tile to its correct position
-     *
      * @return
      */
     public int manhattanDistance() {
-        int sum = 0;
-        for (TilePos p : allTilePos()) {
+        int sum=0;
+        for(TilePos p: allTilePos()) {
             int val = tile(p);
-            if (val > 0) {
+            if( val > 0 ) {
                 TilePos correct = SOLVED.whereIs(val);
-                sum += Math.abs(correct.x = p.x);
-                sum += Math.abs(correct.y = p.y);
+                sum += Math.abs( correct.x = p.x );
+                sum += Math.abs( correct.y = p.y );
             }
         }
         return sum;
@@ -236,7 +244,6 @@ public class FifteenPuzzle {
 
     /**
      * distance heuristic for A*
-     *
      * @return
      */
     public int estimateError() {
@@ -248,57 +255,93 @@ public class FifteenPuzzle {
 
     public List<FifteenPuzzle> allAdjacentPuzzles() {
         ArrayList<FifteenPuzzle> out = new ArrayList<FifteenPuzzle>();
-        for (TilePos move : allValidMoves()) {
-            out.add(moveClone(move));
+        for( TilePos move: allValidMoves() ) {
+            out.add( moveClone(move) );
         }
         return out;
     }
 
+    /**
+     * returns a list of boards if it was able to solve it, or else null
+     * @return
+     */
+    public List<FifteenPuzzle> dijkstraSolve() {
+        Queue<FifteenPuzzle> toVisit = new LinkedList<FifteenPuzzle>();
+        HashMap<FifteenPuzzle,FifteenPuzzle> predecessor = new HashMap<FifteenPuzzle,FifteenPuzzle>();
+        toVisit.add(this);
+        predecessor.put(this, null);
+        int cnt=0;
+        while( toVisit.size() > 0) {
+            FifteenPuzzle candidate = toVisit.remove();
+            cnt++;
+            if( cnt % 10000 == 0) {
+                System.out.printf("Considered %,d positions. Queue = %,d\n", cnt, toVisit.size());
+            }
+            if( candidate.isSolved() ) {
+                System.out.printf("Solution considered %d boards\n", cnt);
+                LinkedList<FifteenPuzzle> solution = new LinkedList<FifteenPuzzle>();
+                FifteenPuzzle backtrace=candidate;
+                while( backtrace != null ) {
+                    solution.addFirst(backtrace);
+                    backtrace = predecessor.get(backtrace);
+                }
+                return solution;
+            }
+            for(FifteenPuzzle fp: candidate.allAdjacentPuzzles()) {
+                if( !predecessor.containsKey(fp) ) {
+                    predecessor.put(fp,candidate);
+                    toVisit.add(fp);
+                }
+            }
+        }
+        return null;
+    }
+
+
 
     /**
      * returns a list of boards if it was able to solve it, or else null
-     *
      * @return
      */
     public List<FifteenPuzzle> aStarSolve() {
-        HashMap<FifteenPuzzle, FifteenPuzzle> predecessor = new HashMap<FifteenPuzzle, FifteenPuzzle>();
-        HashMap<FifteenPuzzle, Integer> depth = new HashMap<FifteenPuzzle, Integer>();
-        final HashMap<FifteenPuzzle, Integer> score = new HashMap<FifteenPuzzle, Integer>();
+        HashMap<FifteenPuzzle,FifteenPuzzle> predecessor = new HashMap<FifteenPuzzle,FifteenPuzzle>();
+        HashMap<FifteenPuzzle,Integer> depth = new HashMap<FifteenPuzzle,Integer>();
+        final HashMap<FifteenPuzzle,Integer> score = new HashMap<FifteenPuzzle,Integer>();
         Comparator<FifteenPuzzle> comparator = new Comparator<FifteenPuzzle>() {
             @Override
             public int compare(FifteenPuzzle a, FifteenPuzzle b) {
                 return score.get(a) - score.get(b);
             }
         };
-        PriorityQueue<FifteenPuzzle> toVisit = new PriorityQueue<FifteenPuzzle>(10000, comparator);
+        PriorityQueue<FifteenPuzzle> toVisit = new PriorityQueue<FifteenPuzzle>(10000,comparator);
 
         predecessor.put(this, null);
-        depth.put(this, 0);
+        depth.put(this,0);
         score.put(this, this.estimateError());
         toVisit.add(this);
-        int cnt = 0;
-        while (toVisit.size() > 0) {
+        int cnt=0;
+        while( toVisit.size() > 0) {
             FifteenPuzzle candidate = toVisit.remove();
             cnt++;
-            if (cnt % 10000 == 0) {
+            if( cnt % 10000 == 0) {
                 System.out.printf("Considered %,d positions. Queue = %,d\n", cnt, toVisit.size());
             }
-            if (candidate.isSolved()) {
+            if( candidate.isSolved() ) {
                 System.out.printf("Solution considered %d boards\n", cnt);
                 LinkedList<FifteenPuzzle> solution = new LinkedList<FifteenPuzzle>();
-                FifteenPuzzle backtrace = candidate;
-                while (backtrace != null) {
+                FifteenPuzzle backtrace=candidate;
+                while( backtrace != null ) {
                     solution.addFirst(backtrace);
                     backtrace = predecessor.get(backtrace);
                 }
                 return solution;
             }
-            for (FifteenPuzzle fp : candidate.allAdjacentPuzzles()) {
-                if (!predecessor.containsKey(fp)) {
-                    predecessor.put(fp, candidate);
-                    depth.put(fp, depth.get(candidate) + 1);
+            for(FifteenPuzzle fp: candidate.allAdjacentPuzzles()) {
+                if( !predecessor.containsKey(fp) ) {
+                    predecessor.put(fp,candidate);
+                    depth.put(fp, depth.get(candidate)+1);
                     int estimate = fp.estimateError();
-                    score.put(fp, depth.get(candidate) + 1 + estimate);
+                    score.put(fp, depth.get(candidate)+1 + estimate);
                     // dont' add to p-queue until the metadata is in place that the comparator needs
                     toVisit.add(fp);
                 }
@@ -308,9 +351,9 @@ public class FifteenPuzzle {
     }
 
     private static void showSolution(List<FifteenPuzzle> solution) {
-        if (solution != null) {
+        if (solution != null ) {
             System.out.printf("Success!  Solution with %d moves:\n", solution.size());
-            for (FifteenPuzzle sp : solution) {
+            for( FifteenPuzzle sp: solution) {
                 sp.show();
             }
         } else {
@@ -329,6 +372,10 @@ public class FifteenPuzzle {
 
         System.out.println("Solving with A*");
         solution = p.aStarSolve();
+        showSolution(solution);
+
+        System.out.println("Solving with Dijkstra");
+        solution = p.dijkstraSolve();
         showSolution(solution);
     }
 
