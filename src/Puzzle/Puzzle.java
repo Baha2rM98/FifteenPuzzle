bbe9ff46c1bfaabb35bf749be500297e1887e3f7
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-01 03:14:41 +0330

    All methods documented, codes cleaned, dijkstra method added

--- src/Puzzle/Puzzle.java
+++ src/Puzzle/Puzzle.java
@@ -8,55 +8,133 @@ import java.util.*;
  * @author baha2r
  * Date: 28/Nov/2019 11:03 PM
  **/
-//TODO => Document
+
 public class Puzzle {
 
-    private final static int DIMS = 4;
+    // Dimension of the puzzle
+    private final static int DIMENSION = 4;
+
+    // State of each node
     private int[][] state;
-    private int display_width;
+
+    // Display Separator
+    private int displaySeparator;
+
+    // Blank Node
     private Node blank;
 
+
+    /**
+     * Constructor
+     */
     public Puzzle() {
-        state = new int[DIMS][DIMS];
-        int cnt = 1;
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                state[i][j] = cnt;
-                cnt++;
+        state = new int[DIMENSION][DIMENSION];
+        int count = 1;
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                this.state[i][j] = count;
+                count++;
             }
         }
-        display_width = Integer.toString(cnt).length();
-        blank = new Node(DIMS - 1, DIMS - 1);
-        state[blank.x][blank.y] = 0;
+        this.displaySeparator = Integer.toString(count).length();
+        blank = new Node(DIMENSION - 1, DIMENSION - 1);
+        this.state[this.blank.x][this.blank.y] = 0;
     }
 
-    private Puzzle(Puzzle toClone) {
+
+    /**
+     * Constructor
+     * <p>
+     * Creates new puzzle with applied movements
+     */
+    private Puzzle(Puzzle toBeCloned) {
         this();
-        for (Node p : allCellPos()) {
-            state[p.x][p.y] = toClone.cell(p);
+        for (Node p : allNodePos()) {
+            this.state[p.x][p.y] = toBeCloned.node(p);
         }
-        blank = toClone.getBlank();
+        this.blank = toBeCloned.getBlank();
     }
 
-    public void getUserInput() {
-        Scanner scn = new Scanner(System.in);
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                state[i][j] = scn.nextInt();
-            }
-        }
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                if (state[i][j] < 0 || state[i][j] > 15)
-                    throw new RuntimeException("Wrong input! Try again...");
-            }
-        }
+
+    /**
+     * Returns value of each node
+     *
+     * @param p Node p
+     * @return Returns value of each node
+     */
+    private int node(Node p) {
+        return this.state[p.x][p.y];
     }
 
-    private List<Node> allCellPos() {
-        ArrayList<Node> out = new ArrayList<Node>();
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
+
+    /**
+     * Returns the blank node
+     *
+     * @return Returns the blank node
+     */
+    private Node getBlank() {
+        return this.blank;
+    }
+
+
+    /**
+     * Returns goal puzzle
+     *
+     * @return Returns goal puzzle to check with each solving puzzle's state
+     */
+    private Puzzle getSOLVED() {
+        return new Puzzle();
+    }
+
+
+    /**
+     * Calculates fScore of each node
+     *
+     * @return Returns fScore of each node
+     */
+    private int numberMisplacedNodes() {
+        Puzzle solved = getSOLVED();
+        int wrong = 0;
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if ((this.state[i][j] > 0) && (this.state[i][j] != solved.state[i][j])) {
+                    wrong++;
+                }
+            }
+        }
+        return wrong;
+    }
+
+
+    /**
+     * Checks if current puzzle is solved
+     *
+     * @return Returns true if puzzle is solved, false otherwise
+     */
+    private boolean isSolved() {
+        return this.numberMisplacedNodes() == 0;
+    }
+
+
+    /**
+     * Calculates fScore of each node
+     *
+     * @return Returns fScore of each node
+     */
+    private int estimateError() {
+        return this.numberMisplacedNodes();
+    }
+
+
+    /**
+     * Returns a list of all positions of nodes
+     *
+     * @return Returns a list of all positions of nodes
+     */
+    private List<Node> allNodePos() {
+        ArrayList<Node> out = new ArrayList<>();
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
                 out.add(new Node(i, j));
             }
         }
@@ -64,38 +142,153 @@ public class Puzzle {
     }
 
 
-    private int cell(Node p) {
-        return state[p.x][p.y];
+    /**
+     * Returns a list of all adjacency of each node
+     *
+     * @return Returns a list of all adjacency of each node
+     */
+    private List<Puzzle> allAdjacentNodes() {
+        ArrayList<Puzzle> out = new ArrayList<>();
+        for (Node move : allValidMoves()) {
+            out.add(movedClone(move));
+        }
+        return out;
     }
 
 
-    private Node getBlank() {
-        return blank;
+    /**
+     * Checks if a movement is valid
+     *
+     * @return Returns true if a movement is valid, false otherwise
+     */
+    private boolean isValidMove(Node p) {
+        if ((p.x < 0) || (p.x >= DIMENSION)) {
+            return false;
+        }
+        if ((p.y < 0) || (p.y >= DIMENSION)) {
+            return false;
+        }
+        int dx = this.blank.x - p.x;
+        int dy = this.blank.y - p.y;
+        return (Math.abs(dx) + Math.abs(dy) == 1) && (dx * dy == 0);
     }
 
 
-    private Node whereIs(int x) {
-        for (Node p : allCellPos()) {
-            if (cell(p) == x) {
-                return p;
+    /**
+     * Returns a list of all valid movements for each node
+     *
+     * @return Returns a list of all valid movements for each node
+     */
+    private List<Node> allValidMoves() {
+        ArrayList<Node> out = new ArrayList<>();
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 2; dy++) {
+                Node tp = new Node(this.blank.x + dx, this.blank.y + dy);
+                if (isValidMove(tp)) {
+                    out.add(tp);
+                }
             }
         }
-        return null;
+        return out;
     }
 
+
+    /**
+     * Moves a node to a new coordination
+     */
+    private void move(Node p) {
+        if (!isValidMove(p)) {
+            throw new RuntimeException("Invalid move");
+        }
+        assert this.state[this.blank.x][this.blank.y] == 0;
+        this.state[this.blank.x][this.blank.y] = this.state[p.x][p.y];
+        this.state[p.x][p.y] = 0;
+        this.blank = p;
+    }
+
+
+    /**
+     * Returns a new puzzle with the applied movement
+     *
+     * @return Returns a new puzzle with the applied movement
+     */
+    private Puzzle movedClone(Node p) {
+        Puzzle out = new Puzzle(this);
+        out.move(p);
+        return out;
+    }
+
+
+    /**
+     * Shuffles goal puzzle as initiation state
+     */
+    public void shuffle(int howMany) {
+        for (int i = 0; i < howMany; i++) {
+            List<Node> possible = allValidMoves();
+            int which = (int) (Math.random() * possible.size());
+            Node move = possible.get(which);
+            this.move(move);
+        }
+    }
+
+//    private int getInvCount() {
+//        List<Integer> stateList = new ArrayList<>();
+//        for (int i = 0; i < DIMENSION; i++) {
+//            for (int j = 0; j < DIMENSION; j++) {
+//                stateList.add(this.state[i][j]);
+//            }
+//        }
+//        int[] arr = new int[stateList.size()];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = stateList.get(i);
+//        }
+//        int invCount = 0;
+//        for (int i = 0; i < DIMENSION * DIMENSION - 1; i++) {
+//            for (int j = i + 1; j < DIMENSION * DIMENSION; j++) {
+////                if ((arr[i] > arr[j]))
+//                    invCount++;
+//            }
+//        }
+//        return invCount;
+//    }
+//
+//
+//    private int findBlankPosition() {
+//        for (int i = DIMENSION - 1; i >= 0; i--)
+//            for (int j = DIMENSION - 1; j >= 0; j--)
+//                if (this.state[i][j] == 0)
+//                    return DIMENSION - i;
+//        return -1;
+//    }
+//
+//
+//    private boolean isSolvable(int[][] state) {
+//        int invCount = getInvCount();
+//        int pos = findBlankPosition();
+//        if ((pos % 2 != 0) && (invCount % 2 == 0))
+//            return true;
+//        if ((pos % 2 == 0) && (invCount % 2 != 0))
+//            return true;
+//        return false;
+//    }
+
+
+    /**
+     * Shows each state as a puzzle
+     */
     public void show() {
         System.out.println("-----------------");
-        for (int i = 0; i < DIMS; i++) {
+        for (int i = 0; i < DIMENSION; i++) {
             System.out.print("| ");
-            for (int j = 0; j < DIMS; j++) {
-                int n = state[i][j];
+            for (int j = 0; j < DIMENSION; j++) {
+                int n = this.state[i][j];
                 StringBuilder s;
                 if (n > 0) {
                     s = new StringBuilder(Integer.toString(n));
                 } else {
                     s = new StringBuilder();
                 }
-                while (s.length() < display_width) {
+                while (s.length() < this.displaySeparator) {
                     s.append(" ");
                 }
                 System.out.print(s + "| ");
@@ -106,148 +299,116 @@ public class Puzzle {
     }
 
 
-    private List<Node> allValidMoves() {
-        ArrayList<Node> out = new ArrayList<Node>();
-        for (int dx = -1; dx < 2; dx++) {
-            for (int dy = -1; dy < 2; dy++) {
-                Node tp = new Node(blank.x + dx, blank.y + dy);
-                if (isValidMove(tp)) {
-                    out.add(tp);
-                }
+    /**
+     * Gets user input as initiation state
+     */
+    public void getUserInput() {
+        Scanner scn = new Scanner(System.in);
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                this.state[i][j] = scn.nextInt();
             }
         }
-        return out;
-    }
-
-
-    private boolean isValidMove(Node p) {
-        if ((p.x < 0) || (p.x >= DIMS)) {
-            return false;
-        }
-        if ((p.y < 0) || (p.y >= DIMS)) {
-            return false;
-        }
-        int dx = blank.x - p.x;
-        int dy = blank.y - p.y;
-        return (Math.abs(dx) + Math.abs(dy) == 1) && (dx * dy == 0);
-    }
-
-
-    private void move(Node p) {
-        if (!isValidMove(p)) {
-            throw new RuntimeException("Invalid move");
-        }
-        assert state[blank.x][blank.y] == 0;
-        state[blank.x][blank.y] = state[p.x][p.y];
-        state[p.x][p.y] = 0;
-        blank = p;
-    }
-
-
-    /**
-     * returns a new puzzle with the move applied
-     */
-    private Puzzle moveClone(Node p) {
-        Puzzle out = new Puzzle(this);
-        out.move(p);
-        return out;
-    }
-
-
-    public void shuffle(int howMany) {
-        for (int i = 0; i < howMany; i++) {
-            List<Node> possible = allValidMoves();
-            int which = (int) (Math.random() * possible.size());
-            Node move = possible.get(which);
-            this.move(move);
-        }
-    }
-
-    private Puzzle getSOLVED() {
-        return new Puzzle();
-    }
-
-    private int numberMisplacedTiles() {
-        Puzzle solved = getSOLVED();
-        int wrong = 0;
-        for (int i = 0; i < DIMS; i++) {
-            for (int j = 0; j < DIMS; j++) {
-                if ((state[i][j] > 0) && (state[i][j] != solved.state[i][j])) {
-                    wrong++;
-                }
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (this.state[i][j] < 0 || this.state[i][j] > 15)
+                    throw new RuntimeException("Wrong input! Try again...");
             }
         }
-        return wrong;
     }
 
-
-    private boolean isSolved() {
-        return numberMisplacedTiles() == 0;
-    }
 
     /**
-     * distance heuristic for A*
-     */
-    private int estimateError() {
-        return this.numberMisplacedTiles();
-    }
-
-
-    private List<Puzzle> allAdjacentPuzzles() {
-        ArrayList<Puzzle> out = new ArrayList<Puzzle>();
-        for (Node move : allValidMoves()) {
-            out.add(moveClone(move));
-        }
-        return out;
-    }
-
-    /**
-     * returns a list of boards if it was able to solve it, or else null
+     * A* algorithm try to solve the puzzle and returns a list of puzzles if it was able to solve it, null otherwise
+     *
+     * @return Returns a list of puzzles if it was able to solve it, null otherwise
      */
     public List<Puzzle> aStarSolver() {
-        Map<Puzzle, Puzzle> parent = new HashMap<Puzzle, Puzzle>();
-        Map<Puzzle, Integer> depth = new HashMap<Puzzle, Integer>();
-        Map<Puzzle, Integer> score = new HashMap<Puzzle, Integer>();
-        Comparator<Puzzle> comparator = new Comparator<>() {
-            @Override
-            public int compare(Puzzle a, Puzzle b) {
-                return score.get(a) - score.get(b);
-            }
-        };
-        Queue<Puzzle> toVisit = new PriorityQueue<Puzzle>(10000, comparator);
+//        if (!isSolvable(this.state))
+//            throw new RuntimeException("Unsolvable!");
+        Map<Puzzle, Puzzle> parent = new HashMap<>();
+        Map<Puzzle, Integer> depth = new HashMap<>();
+        Map<Puzzle, Integer> fScore = new HashMap<>();
+        Comparator<Puzzle> comparator = Comparator.comparingInt(fScore::get);
+        Queue<Puzzle> openList = new PriorityQueue<>(10000, comparator);
         parent.put(this, null);
         depth.put(this, 0);
-        score.put(this, this.estimateError());
-        toVisit.add(this);
-        int cnt = 0;
-        while (toVisit.size() > 0) {
-            Puzzle candidate = toVisit.remove();
-            cnt++;
-            if (cnt % 10000 == 0)
-                System.out.printf("Solution tree has %d nodes. PriorityQueue size is: %d\n", cnt, toVisit.size());
-            if (cnt > 2900000) {
-                System.out.println("Unsolvable!\n");
-                break;
-            }
-            if (candidate.isSolved()) {
-                System.out.printf("Solution tree has %d nodes.\n", cnt);
-                LinkedList<Puzzle> solution = new LinkedList<Puzzle>();
-                Puzzle backtrace = candidate;
-                while (backtrace != null) {
-                    solution.addFirst(backtrace);
-                    backtrace = parent.get(backtrace);
+        fScore.put(this, this.estimateError());
+        openList.add(this);
+        int count = 0;
+        try {
+            while (!openList.isEmpty()) {
+                Puzzle currentNode = openList.remove();
+                count++;
+                if (count % 10000 == 0)
+                    System.out.println(count + " states assumed. PriorityQueue size is: " + openList.size());
+                if (currentNode.isSolved()) {
+                    System.out.println("\n" + count + " states assumed.");
+                    LinkedList<Puzzle> solution = new LinkedList<>();
+                    Puzzle backtrace = currentNode;
+                    while (backtrace != null) {
+                        solution.addFirst(backtrace);
+                        backtrace = parent.get(backtrace);
+                    }
+                    return solution;
                 }
-                return solution;
-            }
-            for (Puzzle fp : candidate.allAdjacentPuzzles()) {
-                if (!parent.containsKey(fp)) {
-                    parent.put(fp, candidate);
-                    depth.put(fp, depth.get(candidate) + 1);
-                    int estimate = fp.estimateError();
-                    score.put(fp, depth.get(candidate) + 1 + estimate);
-                    toVisit.add(fp);
+                for (Puzzle puzzle : currentNode.allAdjacentNodes()) {
+                    if (!parent.containsKey(puzzle)) {
+                        parent.put(puzzle, currentNode);
+                        depth.put(puzzle, depth.get(currentNode) + 1);
+                        int estimate = puzzle.estimateError();
+                        fScore.put(puzzle, depth.get(currentNode) + 1 + estimate);
+                        openList.add(puzzle);
+                    }
                 }
             }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Dijkstra algorithm try to solve the puzzle and returns a list of puzzles if it was able to solve it, null otherwise
+     *
+     * @return Returns a list of puzzles if it was able to solve it, null otherwise
+     */
+    public List<Puzzle> dijkstraSolver() {
+        Queue<Puzzle> openList = new LinkedList<>();
+        HashMap<Puzzle, Puzzle> parent = new HashMap<>();
+        openList.add(this);
+        parent.put(this, null);
+        int count = 0;
+        try {
+            while (!openList.isEmpty()) {
+                Puzzle currentNode = openList.remove();
+                count++;
+                if (count % 10000 == 0)
+                    System.out.println(count + "states assumed. PriorityQueue size is: " + openList.size());
+                if (count > 3000000) {
+                    System.out.println("Unsolvable!\n");
+                    break;
+                }
+                if (currentNode.isSolved()) {
+                    System.out.println("\n" + count + " states assumed.");
+                    LinkedList<Puzzle> solution = new LinkedList<>();
+                    Puzzle backtrace = currentNode;
+                    while (backtrace != null) {
+                        solution.addFirst(backtrace);
+                        backtrace = parent.get(backtrace);
+                    }
+                    return solution;
+                }
+                for (Puzzle puzzle : currentNode.allAdjacentNodes()) {
+                    if (!parent.containsKey(puzzle)) {
+                        parent.put(puzzle, currentNode);
+                        openList.add(puzzle);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
         }
         return null;
     }
