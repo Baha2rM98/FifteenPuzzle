07f3d3a1b103ed641a10e38adc6a68ef72acb8d3
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-01 22:33:43 +0330

    Performance improved

--- src/Puzzle/Puzzle.java
+++ src/Puzzle/Puzzle.java
@@ -28,7 +28,7 @@ public class Puzzle {
      * Constructor
      */
     public Puzzle() {
-        state = new int[DIMENSION][DIMENSION];
+        this.state = new int[DIMENSION][DIMENSION];
         int count = 1;
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
@@ -59,11 +59,11 @@ public class Puzzle {
     /**
      * Returns value of each node
      *
-     * @param p Node p
+     * @param node Node node
      * @return Returns value of each node
      */
-    private int nodeVal(Node p) {
-        return this.state[p.x][p.y];
+    private int nodeVal(Node node) {
+        return this.state[node.x][node.y];
     }
 
 
@@ -147,7 +147,7 @@ public class Puzzle {
      * @return Returns true if puzzle is solved, false otherwise
      */
     private boolean isSolved() {
-        return this.numberMisplacedNodes() == 0;
+        return this.manhattanDistance() == 0;
     }
 
 
@@ -197,15 +197,15 @@ public class Puzzle {
      *
      * @return Returns true if a movement is valid, false otherwise
      */
-    private boolean isValidMove(Node p) {
-        if ((p.x < 0) || (p.x >= DIMENSION)) {
+    private boolean isValidMove(Node node) {
+        if ((node.x < 0) || (node.x >= DIMENSION)) {
             return false;
         }
-        if ((p.y < 0) || (p.y >= DIMENSION)) {
+        if ((node.y < 0) || (node.y >= DIMENSION)) {
             return false;
         }
-        int dx = this.blank.x - p.x;
-        int dy = this.blank.y - p.y;
+        int dx = this.blank.x - node.x;
+        int dy = this.blank.y - node.y;
         return (Math.abs(dx) + Math.abs(dy) == 1) && (dx * dy == 0);
     }
 
@@ -219,9 +219,9 @@ public class Puzzle {
         ArrayList<Node> out = new ArrayList<>();
         for (int dx = -1; dx < 2; dx++) {
             for (int dy = -1; dy < 2; dy++) {
-                Node tp = new Node(this.blank.x + dx, this.blank.y + dy);
-                if (isValidMove(tp)) {
-                    out.add(tp);
+                Node node = new Node(this.blank.x + dx, this.blank.y + dy);
+                if (isValidMove(node)) {
+                    out.add(node);
                 }
             }
         }
@@ -232,14 +232,14 @@ public class Puzzle {
     /**
      * Moves a node to a new coordination
      */
-    private void move(Node p) {
-        if (!isValidMove(p)) {
+    private void move(Node node) {
+        if (!isValidMove(node)) {
             throw new RuntimeException("Invalid move");
         }
         assert this.state[this.blank.x][this.blank.y] == 0;
-        this.state[this.blank.x][this.blank.y] = this.state[p.x][p.y];
-        this.state[p.x][p.y] = 0;
-        this.blank = p;
+        this.state[this.blank.x][this.blank.y] = this.state[node.x][node.y];
+        this.state[node.x][node.y] = 0;
+        this.blank = node;
     }
 
 
@@ -266,48 +266,6 @@ public class Puzzle {
             this.move(move);
         }
     }
-
-
-//    private int getInvCount() {
-//        List<Integer> stateList = new ArrayList<>();
-//        for (int i = 0; i < DIMENSION; i++) {
-//            for (int j = 0; j < DIMENSION; j++) {
-//                stateList.add(this.state[i][j]);
-//            }
-//        }
-//        int[] arr = new int[stateList.size()];
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = stateList.get(i);
-//        }
-//        int invCount = 0;
-//        for (int i = 0; i < DIMENSION * DIMENSION - 1; i++) {
-//            for (int j = i + 1; j < DIMENSION * DIMENSION; j++) {
-////                if ((arr[i] > arr[j]))
-//                    invCount++;
-//            }
-//        }
-//        return invCount;
-//    }
-//
-//
-//    private int findBlankPosition() {
-//        for (int i = DIMENSION - 1; i >= 0; i--)
-//            for (int j = DIMENSION - 1; j >= 0; j--)
-//                if (this.state[i][j] == 0)
-//                    return DIMENSION - i;
-//        return -1;
-//    }
-//
-//
-//    private boolean isSolvable(int[][] state) {
-//        int invCount = getInvCount();
-//        int pos = findBlankPosition();
-//        if ((pos % 2 != 0) && (invCount % 2 == 0))
-//            return true;
-//        if ((pos % 2 == 0) && (invCount % 2 != 0))
-//            return true;
-//        return false;
-//    }
 
 
     /**
@@ -340,16 +298,15 @@ public class Puzzle {
      * Gets user input as initiation state
      */
     public void getUserInput() {
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                this.state[i][j] = -1;
+            }
+        }
         Scanner scn = new Scanner(System.in);
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
                 this.state[i][j] = scn.nextInt();
-            }
-        }
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                if (this.state[i][j] < 0 || this.state[i][j] > 15)
-                    throw new RuntimeException("Wrong input! Try again...");
             }
         }
     }
@@ -361,8 +318,6 @@ public class Puzzle {
      * @return Returns a list of puzzles if it was able to solve it, null otherwise
      */
     public List<Puzzle> aStarSolver() {
-//        if (!isSolvable(this.state))
-//            throw new RuntimeException("Unsolvable!");
         Map<Puzzle, Puzzle> parent = new HashMap<>();
         Map<Puzzle, Integer> depth = new HashMap<>();
         Map<Puzzle, Integer> fScore = new HashMap<>();
